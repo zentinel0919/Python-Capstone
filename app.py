755bdb41b88f88bb7d8703a5d3c652e54cb8087e
Harvey <mhnatividad@tip.edu.ph>
2023-12-03 17:02:14 +0800

    Update API

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ conversation_history = [
 ]
 
 # Load your OpenAI API key from an environment variable
-openai.api_key = os.getenv('OPENAI_API_KEY', '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')
+openai.api_key = os.getenv('OPENAI_API_KEY', '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')
 
 @app.route('/openai', methods=['POST'])
 def call_openai():
